2557c43073dd4743de153d550818e763d5269a25
Mayssam-Khlif <ms.khlif@gmail.com>
2024-10-02 14:56:28 +0100

    Migrate host parser tests

---
+++ test/src/parser/host.cc
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+#include <uri/uri.h>
+
+
+TEST(UriTests, ParseRegistredHost) {
+    ASSERT_EQ(prism::Uri::parseHost("user:password@www.amazon.co.uk:4523"), "www.amazon.co.uk");
+    ASSERT_EQ(prism::Uri::parseHost("user:password@www.amazon.co.uk"), "www.amazon.co.uk");
+
+    ASSERT_EQ(prism::Uri::parseHost("www.amazon.co.uk:4523"), "www.amazon.co.uk");
+    ASSERT_EQ(prism::Uri::parseHost("www.amazon.co.uk"), "www.amazon.co.uk");
+}
+
+TEST(UriTests, ParseHostIPv4) {
+    ASSERT_EQ(prism::Uri::parseHost("user:password@127.0.0.1:4523"), "127.0.0.1");
+    ASSERT_EQ(prism::Uri::parseHost("user:password@127.0.0.1"), "127.0.0.1");
+
+    ASSERT_EQ(prism::Uri::parseHost("127.0.0.1:4523"), "127.0.0.1");
+    ASSERT_EQ(prism::Uri::parseHost("127.0.0.1"), "127.0.0.1");
+}
+
+TEST(UriTests, ParseHostIPv6) {
+    ASSERT_EQ(prism::Uri::parseHost("user:password@[2001:db8::1]:8080"), "[2001:db8::1]");
+    ASSERT_EQ(prism::Uri::parseHost("user:password@[2001:db8::1]"), "[2001:db8::1]");
+
+    ASSERT_EQ(prism::Uri::parseHost("[2001:db8::1]:8080"), "[2001:db8::1]");
+    ASSERT_EQ(prism::Uri::parseHost("[2001:db8::1]"), "[2001:db8::1]");
+}
