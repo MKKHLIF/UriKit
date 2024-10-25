9d316b4849a2b3b77a6afe856436dac03b55a1b8
Mayssam-Khlif <khlif.mk@proton.me>
2024-10-25 20:33:48 +0100

    Initial autority tests

--- test/src/parser/authority.cc
+++ test/src/parser/authority.cc
@@ -4,15 +4,23 @@
 
 TEST(StaticParseHelpers, ParseAuthorityWithPath)
 {
-    // constexpr size_t cursor = 0;
-    //
-    // auto result = Parser::parseAuthority("https://www.google.com/search?q=uri", cursor);
-    // ASSERT_TRUE(result.error);
-    // ASSERT_EQ(result.content, "www.google.com");
-    //
-    // result = Parser::parseAuthority("https://user:password@www.example.com:8080/path", cursor);
-    // ASSERT_TRUE(result.error);
-    // ASSERT_EQ(result.content, "user:password@www.example.com:8080");
+
+    const Uri uri;
+    bool success = uri.parse("https://www.google.com/search?q=uri");
+    ASSERT_TRUE(success);
+    ASSERT_TRUE(uri.hasAuthority());
+
+    ASSERT_EQ(uri.getHost(), "www.google.com");
+    ASSERT_FALSE(uri.hasPort());
+
+    success = uri.parse("https://user:password@www.example.com:8080/path");
+    ASSERT_TRUE(success);
+    ASSERT_TRUE(uri.hasAuthority());
+
+    ASSERT_EQ(uri.getUserInfo(), "user:password");
+    ASSERT_EQ(uri.getHost(), "www.example.com");
+    ASSERT_TRUE(uri.hasPort());
+    ASSERT_EQ(uri.getPort(), 8080);
 }
 
 TEST(StaticParseHelpers, ParseAuthorityWithQuery)
--- uri/src/parser_engine.cc
+++ uri/src/parser_engine.cc
@@ -13,54 +13,84 @@ public:
     {
     };
     ~Imp() = default;
-    bool parse(const std::string& str_rep, const Uri* obj);
+    bool parse(const std::string& str, const Uri* obj);
 
 private:
     size_t cursor;
 
-    ParseResult<std::string> parseScheme(const std::string& str_rep);
-    [[nodiscard]] ParseResult<std::string> parseAuthority(const std::string& str_rep);
-    // [[nodiscard]] ParseResult<std::string> parseUserInfo(const std::string& str_rep);
-    // [[nodiscard]] ParseResult<std::string> parseHost(const std::string& str_rep);
-    // [[nodiscard]] ParseResult<std::string> parsePort(const std::string& str_rep);
-    // [[nodiscard]] ParseResult<std::vector<std::string>> parsePath(const std::string& str_rep);
+    ParseResult<std::string> parseScheme(const std::string& str);
+    [[nodiscard]] ParseResult<std::string> parseAuthority(const std::string& str);
+    // [[nodiscard]] ParseResult<std::string> parseUserInfo(const std::string& authority);
+    // [[nodiscard]] ParseResult<std::string> parseHost(const std::string& authority);
+    // [[nodiscard]] ParseResult<std::string> parsePort(const std::string& authority);
+    // [[nodiscard]] ParseResult<std::vector<std::string>> parsePath(const std::string& path);
 };
 
-bool Parser::Imp::parse(const std::string& str_rep, const Uri* obj)
+bool Parser::Imp::parse(const std::string& str, const Uri* obj)
 {
     
-    auto result = parseScheme(str_rep);
+    auto result = parseScheme(str);
     if (result.error) return false;
 
     if (!result.content.empty()) {
         obj->setScheme(result.content);
-    }
+        // skip // if exists
+        if (cursor + 1 < str.length() && str[cursor] == '/' && str[cursor + 1] == '/')
+        {
+            cursor += 2;
+        }
+        // "www.google.com/search?q=uri"
+        // "/C:/Users/Example/Documents/Projects/2024/Report.pdf"
+        // "user@example.com"
 
-    result = parseAuthority(str_rep);
+        auto path_end = str.find_first_of("?#");
+        if (path_end == std::string::npos) path_end = str.length();
+        const auto authority_and_path = str.substr(cursor, path_end);
+
+        // split authority from path
+        auto authority_end = authority_and_path.find('/');
+        if (authority_end == std::string::npos) {
+            authority_end = authority_and_path.length();
+        }
+
+        auto authority = authority_and_path.substr(cursor, authority_end);
+        auto path = authority_and_path.substr(authority_end);
+
+        result = parseAuthority(authority);
+        if (result.error) return false;
+
+        result = parseAuthority(path);
+        if (result.error) return false;
+
+    }
+    // relative uri
+    else {
+
+    }
 
     return true;
 }
 
-ParseResult<std::string> Parser::Imp::parseScheme(const std::string& str_rep)
+ParseResult<std::string> Parser::Imp::parseScheme(const std::string& str)
 {
     // "https://www.google.com/search?q=uri"
     // "file:///C:/Users/Example/Documents/Projects/2024/Report.pdf"
     // "mailto:user@example.com"
     // "foo/bar"
 
-    auto authority_or_path_start = str_rep.find('/');
+    auto authority_or_path_start = str.find('/');
 
     if (authority_or_path_start == std::string::npos)
     {
-        authority_or_path_start = str_rep.length();
+        authority_or_path_start = str.length();
     }
 
-    const auto scheme_end = str_rep.substr(cursor, authority_or_path_start).find(':');
+    const auto scheme_end = str.substr(cursor, authority_or_path_start).find(':');
 
     if (scheme_end == std::string::npos) return {false, ""};
 
 
-    std::string scheme = str_rep.substr(cursor, scheme_end);
+    std::string scheme = str.substr(cursor, scheme_end);
 
     scheme = StringExtensions::lowercase(scheme);
 
@@ -73,14 +103,10 @@ ParseResult<std::string> Parser::Imp::parseScheme(const std::string& str_rep)
     return result;
 }
 
-ParseResult<std::string> Parser::Imp::parseAuthority(const std::string& str_rep)
+ParseResult<std::string> Parser::Imp::parseAuthority(const std::string& str)
 {
-    // "//www.google.com/search?q=uri"
-    // "///C:/Users/Example/Documents/Projects/2024/Report.pdf"
-    // "user@example.com"
-    // "foo/bar"
 
-    if (str_rep[cursor] != ':') return {false, ""};
+    return {false, ""};
 }
 
 Parser::Parser(): imp(std::make_unique<Imp>())
