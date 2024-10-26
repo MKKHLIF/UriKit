beafb4c360162fd22f3be1148e74584e024cb2d1
Mayssam-Khlif <khlif.mk@proton.me>
2024-10-26 13:34:03 +0100

    Add essential char sets for later parsing

--- uri/src/character_sets.cc
+++ uri/src/character_sets.cc
@@ -1,6 +1,57 @@
 #include "character.h"
 #include "character_sets.h"
 
-const CharacterSet CharacterSets::ALPHA{CharacterSet('a', 'z'), CharacterSet('A', 'Z')};
+const CharacterSet CharacterSets::ALPHA{
+    CharacterSet('a', 'z'),
+    CharacterSet('A', 'Z')
+};
 const CharacterSet CharacterSets::DIGIT('0', '9');
-const CharacterSet CharacterSets::SCHEME_NOT_FIRST{ALPHA, DIGIT, '+', '-', '.',};
+const CharacterSet CharacterSets::SCHEME_NOT_FIRST{
+    ALPHA,
+    DIGIT,
+    '+', '-', '.',
+};
+const CharacterSet CharacterSets::HEXDIG{
+    CharacterSet('0', '9'),
+    CharacterSet('A', 'F'),
+    CharacterSet('a', 'f')
+};
+const CharacterSet CharacterSets::UNRESERVED{
+    ALPHA,
+    DIGIT,
+    '-', '.', '_', '~'
+};
+const CharacterSet CharacterSets::SUB_DELIMS{
+    '!', '$', '&', '\'', '(', ')',
+    '*', '+', ',', ';', '='
+};
+const CharacterSet CharacterSets::PCHAR_NOT_PCT_ENCODED{
+    UNRESERVED,
+    SUB_DELIMS,
+    ':', '@'
+};
+const CharacterSet CharacterSets::QUERY_OR_FRAGMENT_NOT_PCT_ENCODED{
+    PCHAR_NOT_PCT_ENCODED,
+    '/', '?'
+};
+const CharacterSet CharacterSets::QUERY_NOT_PCT_ENCODED_WITHOUT_PLUS{
+    UNRESERVED,
+    '!', '$', '&', '\'', '(', ')',
+    '*', ',', ';', '=',
+    ':', '@',
+    '/', '?'
+};
+const CharacterSet CharacterSets::USER_INFO_NOT_PCT_ENCODED{
+    UNRESERVED,
+    SUB_DELIMS,
+    ':',
+};
+const CharacterSet CharacterSets::REG_NAME_NOT_PCT_ENCODED{
+    UNRESERVED,
+    SUB_DELIMS
+};
+const CharacterSet CharacterSets::IPV_FUTURE_LAST_PART{
+    UNRESERVED,
+    SUB_DELIMS,
+    ':'
+};
--- uri/src/character_sets.h
+++ uri/src/character_sets.h
@@ -6,5 +6,17 @@ public:
     static const CharacterSet ALPHA;
     static const CharacterSet DIGIT;
     static const CharacterSet SCHEME_NOT_FIRST;
+
+    static const CharacterSet HEXDIG;
+    static const CharacterSet UNRESERVED;
+    static const CharacterSet SUB_DELIMS;
+
+    static const CharacterSet PCHAR_NOT_PCT_ENCODED;
+    static const CharacterSet QUERY_OR_FRAGMENT_NOT_PCT_ENCODED;
+    static const CharacterSet QUERY_NOT_PCT_ENCODED_WITHOUT_PLUS;
+    static const CharacterSet USER_INFO_NOT_PCT_ENCODED;
+    static const CharacterSet REG_NAME_NOT_PCT_ENCODED;
+
+    static const CharacterSet IPV_FUTURE_LAST_PART;
 };
 
