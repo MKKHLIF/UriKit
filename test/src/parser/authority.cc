bca09f56d3c8d101b83a0b4d3f1240b2b2c800bd
Mayssam-Khlif <khlif.mk@proton.me>
2024-10-26 12:08:21 +0100

    rewrite authority tests

--- test/src/parser/authority.cc
+++ test/src/parser/authority.cc
@@ -3,88 +3,145 @@
 
 
 TEST(StaticParseHelpers, ParseAuthorityWithPath) {
-    // const Uri uri;
-    // bool success = uri.parse("https://www.google.com/search?q=uri");
-    // ASSERT_TRUE(success);
-    // ASSERT_TRUE(uri.hasAuthority());
-    //
-    // ASSERT_EQ(uri.getHost(), "www.google.com");
-    // ASSERT_FALSE(uri.hasPort());
-    //
-    // success = uri.parse("https://user:password@www.example.com:8080/path");
-    // ASSERT_TRUE(success);
-    // ASSERT_TRUE(uri.hasAuthority());
-    //
-    // ASSERT_EQ(uri.getUserInfo(), "user:password");
-    // ASSERT_EQ(uri.getHost(), "www.example.com");
-    // ASSERT_TRUE(uri.hasPort());
-    // ASSERT_EQ(uri.getPort(), 8080);
+    const Uri uri;
+    bool success = uri.parse("https://www.google.com/search?q=uri");
+    ASSERT_TRUE(success);
+    ASSERT_TRUE(uri.hasAuthority());
+
+    ASSERT_EQ(uri.getHost(), "www.google.com");
+    ASSERT_FALSE(uri.hasPort());
+
+    success = uri.parse("https://user:password@www.example.com:8080/path");
+    ASSERT_TRUE(success);
+    ASSERT_TRUE(uri.hasAuthority());
+
+    ASSERT_EQ(uri.getUserInfo(), "user:password");
+    ASSERT_EQ(uri.getHost(), "www.example.com");
+    ASSERT_TRUE(uri.hasPort());
+    ASSERT_EQ(uri.getPort(), 8080);
 }
 
 TEST(StaticParseHelpers, ParseAuthorityWithQuery) {
-    // constexpr size_t cursor = 0;
-    //
-    // auto result = Parser::parseAuthority("https://user:password@www.example.com:8080?query", cursor);
-    // ASSERT_TRUE(result.error);
-    // ASSERT_EQ(result.content, "user:password@www.example.com:8080");
+    const Uri uri;
+    bool success = uri.parse("https://www.google.com/search?q=uri");
+    ASSERT_TRUE(success);
+    ASSERT_TRUE(uri.hasAuthority());
+    ASSERT_EQ(uri.getHost(), "www.google.com");
+    ASSERT_FALSE(uri.hasPort());
+
+    success = uri.parse("https://user:password@www.example.com:8080?query");
+    ASSERT_TRUE(success);
+    ASSERT_TRUE(uri.hasAuthority());
+    ASSERT_EQ(uri.getUserInfo(), "user:password");
+    ASSERT_EQ(uri.getHost(), "www.example.com");
+    ASSERT_TRUE(uri.hasPort());
+    ASSERT_EQ(uri.getPort(), 8080);
 }
 
 TEST(StaticParseHelpers, ParseAuthorityWithFragment) {
-    // constexpr size_t cursor = 0;
-    //
-    // auto result = Parser::parseAuthority("https://user:password@www.example.com:8080#fragment", cursor);
-    // ASSERT_TRUE(result.error);
-    // ASSERT_EQ(result.content, "user:password@www.example.com:8080");
+    const Uri uri;
+
+    const bool success = uri.parse("https://user:password@www.example.com:8080#fragment");
+    ASSERT_TRUE(success);
+    ASSERT_TRUE(uri.hasAuthority());
+    ASSERT_EQ(uri.getUserInfo(), "user:password");
+    ASSERT_EQ(uri.getHost(), "www.example.com");
+    ASSERT_TRUE(uri.hasPort());
+    ASSERT_EQ(uri.getPort(), 8080);
 }
 
 TEST(StaticParseHelpers, ParseAuthorityOnly) {
-    // constexpr size_t cursor = 0;
-    //
-    // auto result = Parser::parseAuthority("https://www.example.com:8080", cursor);
-    // ASSERT_TRUE(result.error);
-    // ASSERT_EQ(result.content, "www.example.com:8080");
-    //
-    // result = Parser::parseAuthority("https://www.example.com", cursor);
-    // ASSERT_TRUE(result.error);
-    // ASSERT_EQ(result.content, "www.example.com");
-    //
-    // result = Parser::parseAuthority("https://user:password@www.example.com:8080", cursor);
-    // ASSERT_TRUE(result.error);
-    // ASSERT_EQ(result.content, "user:password@www.example.com:8080");
-    //
-    // result = Parser::parseAuthority("https://user:password@www.example.com", cursor);
-    // ASSERT_TRUE(result.error);
-    // ASSERT_EQ(result.content, "user:password@www.example.com");
+    const Uri uri;
+    bool success = uri.parse("https://www.google.com");
+    ASSERT_TRUE(success);
+    ASSERT_TRUE(uri.hasAuthority());
+    ASSERT_EQ(uri.getHost(), "www.google.com");
+    ASSERT_FALSE(uri.hasPort());
+
+    success = uri.parse("https://user:password@www.example.com:8080");
+    ASSERT_TRUE(success);
+    ASSERT_TRUE(uri.hasAuthority());
+    ASSERT_EQ(uri.getUserInfo(), "user:password");
+    ASSERT_EQ(uri.getHost(), "www.example.com");
+    ASSERT_TRUE(uri.hasPort());
+    ASSERT_EQ(uri.getPort(), 8080);
 }
 
+TEST(StaticParseHelpers, ParseAuthorityHostIPv4) {
+    const Uri uri;
+    bool success = uri.parse("https://user:password@127.0.0.1:4523");
+    ASSERT_TRUE(success);
+    ASSERT_TRUE(uri.hasAuthority());
+    ASSERT_EQ(uri.getUserInfo(), "user:password");
+    ASSERT_EQ(uri.getHost(), "127.0.0.1");
+    ASSERT_TRUE(uri.hasPort());
+    ASSERT_EQ(uri.getPort(), 4523);
+
+    success = uri.parse("https://user:password@127.0.0.1");
+    ASSERT_TRUE(success);
+    ASSERT_TRUE(uri.hasAuthority());
+    ASSERT_EQ(uri.getUserInfo(), "user:password");
+    ASSERT_EQ(uri.getHost(), "127.0.0.1");
+    ASSERT_FALSE(uri.hasPort());
+}
+
+TEST(StaticParseHelpers, ParseHostIPv6) {
+    const Uri uri;
+    bool success = uri.parse("https://[2001:db8::1]:8080");
+    ASSERT_TRUE(success);
+    ASSERT_TRUE(uri.hasAuthority());
+    ASSERT_EQ(uri.getHost(), "[2001:db8::1]");
+    ASSERT_TRUE(uri.hasPort());
+    ASSERT_EQ(uri.getPort(), 8080);
+
+    success = uri.parse("https://user:password@[2001:db8::1]:8080");
+    ASSERT_TRUE(success);
+    ASSERT_TRUE(uri.hasAuthority());
+    ASSERT_EQ(uri.getUserInfo(), "user:password");
+    ASSERT_EQ(uri.getHost(), "[2001:db8::1]");
+    ASSERT_TRUE(uri.hasPort());
+    ASSERT_EQ(uri.getPort(), 8080);
+
+    success = uri.parse("https://[2001:db8::1]");
+    ASSERT_TRUE(success);
+    ASSERT_TRUE(uri.hasAuthority());
+    ASSERT_EQ(uri.getHost(), "[2001:db8::1]");
+    ASSERT_FALSE(uri.hasPort());
+}
+
+
 TEST(StaticParseHelpers, ParseAuthorityWithUnusualCharacters) {
-    // constexpr size_t cursor = 0;
-    //
-    // auto result = Parser::parseAuthority("https://www.amazon.co.uk:80#?/", cursor);
-    // ASSERT_TRUE(result.error);
-    // ASSERT_EQ(result.content, "www.amazon.co.uk:80");
-    //
-    // result = Parser::parseAuthority("https://www.amazon.co.uk:80", cursor);
-    // ASSERT_TRUE(result.error);
-    // ASSERT_EQ(result.content, "www.amazon.co.uk:80");
+    const Uri uri;
+    bool success = uri.parse("https://www.amazon.co.uk:80#?/");
+    ASSERT_TRUE(success);
+    ASSERT_TRUE(uri.hasAuthority());
+    ASSERT_EQ(uri.getHost(), "www.amazon.co.uk");
+    ASSERT_TRUE(uri.hasPort());
+    ASSERT_EQ(uri.getPort(), 80);
+
+    success = uri.parse("https://www.amazon.co.uk:80");
+    ASSERT_TRUE(success);
+    ASSERT_TRUE(uri.hasAuthority());
+    ASSERT_EQ(uri.getHost(), "www.amazon.co.uk");
+    ASSERT_TRUE(uri.hasPort());
+    ASSERT_EQ(uri.getPort(), 80);
 }
 
 TEST(StaticParseHelpers, ParseAuthorityWithoutAuthority) {
-    // constexpr size_t cursor = 0;
-    //
-    // auto result = Parser::parseAuthority("mailto:user@example.com", cursor);
-    // ASSERT_FALSE(result.error);
-    // ASSERT_EQ(result.content, "");
-    //
-    // result = Parser::parseAuthority("file:///C:/Users/Example/Documents/Projects/2024/Report.pdf", cursor);
-    // ASSERT_FALSE(result.error);
-    // ASSERT_EQ(result.content, "");
+    const Uri uri;
+    bool success = uri.parse("mailto:user@example.com");
+    ASSERT_TRUE(success);
+    ASSERT_FALSE(uri.hasAuthority());
+
+    success = uri.parse("file:///C:/Users/Example/Documents/Projects/2024/Report.pdf");
+    ASSERT_TRUE(success);
+    ASSERT_FALSE(uri.hasAuthority());
 }
 
 TEST(StaticParseHelpers, ParseAuthorityForNetworkLocation) {
-    // constexpr size_t cursor = 0;
-    //
-    // auto result = Parser::parseAuthority("file://server/share/folder/file.txt", cursor);
-    // ASSERT_TRUE(result.error);
-    // ASSERT_EQ(result.content, "server");
+    const Uri uri;
+    bool success = uri.parse("file://server/share/folder/file.txt");
+    ASSERT_TRUE(success);
+    ASSERT_TRUE(uri.hasAuthority());
+    ASSERT_EQ(uri.getHost(), "server");
 }
