a97c891336c4ff4f31465053ee921a1ae78edad1
Mayssam-Khlif <khlif.mk@proton.me>
2024-10-26 12:08:45 +0100

    remove useless tests

--- test/src/parser/host.cc
+++ test/src/parser/host.cc
@@ -2,8 +2,7 @@
 #include <uri/uri.h>
 #include "parser_engine.h"
 
-TEST(StaticParseHelpers, ParseRegisteredHost)
-{
+TEST(StaticParseHelpers, ParseRegisteredHost) {
     // constexpr size_t cursor = 0;
     //
     // auto result = Parser::parseHost("user:password@www.amazon.co.uk:4523", cursor);
@@ -23,44 +22,4 @@ TEST(StaticParseHelpers, ParseRegisteredHost)
     // ASSERT_EQ(result.content, "www.amazon.co.uk");
 }
 
-TEST(StaticParseHelpers, ParseHostIPv4)
-{
-    // constexpr size_t cursor = 0;
-    //
-    // auto result = Parser::parseHost("user:password@127.0.0.1:4523", cursor);
-    // ASSERT_TRUE(result.error);
-    // ASSERT_EQ(result.content, "127.0.0.1");
-    //
-    // result = Parser::parseHost("user:password@127.0.0.1", cursor);
-    // ASSERT_TRUE(result.error);
-    // ASSERT_EQ(result.content, "127.0.0.1");
-    //
-    // result = Parser::parseHost("127.0.0.1:4523", cursor);
-    // ASSERT_TRUE(result.error);
-    // ASSERT_EQ(result.content, "127.0.0.1");
-    //
-    // result = Parser::parseHost("127.0.0.1", cursor);
-    // ASSERT_TRUE(result.error);
-    // ASSERT_EQ(result.content, "127.0.0.1");
-}
 
-TEST(StaticParseHelpers, ParseHostIPv6)
-{
-    // constexpr size_t cursor = 0;
-    //
-    // auto result = Parser::parseHost("user:password@[2001:db8::1]:8080", cursor);
-    // ASSERT_TRUE(result.error);
-    // ASSERT_EQ(result.content, "[2001:db8::1]");
-    //
-    // result = Parser::parseHost("user:password@[2001:db8::1]", cursor);
-    // ASSERT_TRUE(result.error);
-    // ASSERT_EQ(result.content, "[2001:db8::1]");
-    //
-    // result = Parser::parseHost("[2001:db8::1]:8080", cursor);
-    // ASSERT_TRUE(result.error);
-    // ASSERT_EQ(result.content, "[2001:db8::1]");
-    //
-    // result = Parser::parseHost("[2001:db8::1]", cursor);
-    // ASSERT_TRUE(result.error);
-    // ASSERT_EQ(result.content, "[2001:db8::1]");
-}
