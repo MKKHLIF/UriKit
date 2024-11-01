f65860c8cf4a9d6751dbbe67ed692299413aff6b
Mayssam-Khlif <khlif.mk@proton.me>
2024-11-01 17:08:10 +0100

    refactor uri generation files

--- uri/src/gen.cc
+++ uri/src/gen.cc
@@ -1,13 +1,11 @@
 #include "gen.h"
 
-std::string URIGenerator::generate(const Uri& uri)
-{
+std::string URIGenerator::generate(const Uri &uri) {
 	std::string result;
 	result += uri.getScheme();
 	result += "://";
 	result += uri.getHost();
-	for (const auto& path : uri.getPath())
-	{
+	for (const auto &path: uri.getPath()) {
 		result += "/";
 		result += path;
 	}
