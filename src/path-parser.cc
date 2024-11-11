e41fbde3228f4e80206abc9d2d18f94fc865891d
Mayssam-Khlif <khlif.mk@proton.me>
2024-11-11 21:51:58 +0100

    (#14) path parser refactor

--- src/path-parser.cc
+++ src/path-parser.cc
@@ -3,26 +3,28 @@
 bool PathParser::parse(std::string &uri, std::vector<std::string> &path) {
     path.clear();
 
+    // Special case of a path that is empty but needs a single
+    // empty-string element to indicate that it is absolute.
     if (uri == "/") {
-        // Special case of a path that is empty but needs a single
-        // empty-string element to indicate that it is absolute.
-        path.push_back("");
-    } else if (!uri.empty()) {
+        path.emplace_back("");
+        return true;
+    }
+    if (!uri.empty()) {
         for (;;) {
-            auto path_delimiter = uri.find('/');
+            const auto path_delimiter = uri.find('/');
             if (path_delimiter == std::string::npos) {
                 path.push_back(uri);
                 break;
             }
             path.emplace_back(
                 uri.begin(),
-                uri.begin() + path_delimiter
+                uri.begin() + static_cast<long long>(path_delimiter)
             );
             uri = uri.substr(path_delimiter + 1);
         }
         // for (auto& segment: path) {
         //     if (!DecodeElement(segment, PCHAR_NOT_PCT_ENCODED)) {
-        //         return {true, result};
+        //         return false;
         //     }
         // }
     }
