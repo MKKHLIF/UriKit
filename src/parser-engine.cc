63cab8b5eaf1b686d3edd74a56d76be7126d434a
Mayssam-Khlif <khlif.mk@proton.me>
2024-11-11 12:00:46 +0100

    minor refactor

--- src/parser-engine.cc
+++ src/parser-engine.cc
@@ -37,50 +37,48 @@ bool Parser::Imp::parse(std::string &uri, const Uri *obj) {
         return true;
     }
 
-    if (!content.empty()) {
-        obj->setScheme(content);
+    obj->setScheme(content);
 
-        auto path_end = uri.find_first_of("?#");
-        if (path_end == std::string::npos) path_end = uri.length();
-        const auto authority_and_path = uri.substr(0, path_end);
+    auto path_end = uri.find_first_of("?#");
+    if (path_end == std::string::npos) path_end = uri.length();
+    const auto authority_and_path = uri.substr(0, path_end);
 
-        // split authority from path
-        auto authority_end = authority_and_path.find('/');
-        if (authority_end == std::string::npos) {
-            authority_end = authority_and_path.length();
-        }
-
-        auto authority = authority_and_path.substr(0, authority_end);
-        auto path = authority_and_path.substr(authority_end);
-
-        if (obj->getScheme() == "mailto") {
-            path = authority;
-            authority.clear();
-        }
-
-        std::vector<std::string> components(3);
-        status = AuthorityParser::parse(authority, components);
-        if (status == false) {
-            obj->reset();
-            return false;
-        }
-        if (!components[0].empty()) {
-            obj->setUserInfo(components[0]);
-        }
-        if (!components[1].empty()) {
-            obj->setHost(components[1]);
-        }
-        if (!components[2].empty()) {
-            obj->setPort(std::stoi(components[2]));
-        }
-        std::vector<std::string> path_components;
-        status = PathParser::parse(path, path_components);
-        if (status == false) {
-            obj->reset();
-            return false;
-        }
-        obj->setPath(path_components);
+    // split authority from path
+    auto authority_end = authority_and_path.find('/');
+    if (authority_end == std::string::npos) {
+        authority_end = authority_and_path.length();
     }
+
+    auto authority = authority_and_path.substr(0, authority_end);
+    auto path = authority_and_path.substr(authority_end);
+
+    if (obj->getScheme() == "mailto") {
+        path = authority;
+        authority.clear();
+    }
+
+    std::vector<std::string> components(3);
+    status = AuthorityParser::parse(authority, components);
+    if (status == false) {
+        obj->reset();
+        return false;
+    }
+    if (!components[0].empty()) {
+        obj->setUserInfo(components[0]);
+    }
+    if (!components[1].empty()) {
+        obj->setHost(components[1]);
+    }
+    if (!components[2].empty()) {
+        obj->setPort(std::stoi(components[2]));
+    }
+    std::vector<std::string> path_components;
+    status = PathParser::parse(path, path_components);
+    if (status == false) {
+        obj->reset();
+        return false;
+    }
+    obj->setPath(path_components);
 
 
     return true;
