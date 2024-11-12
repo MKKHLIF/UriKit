b8a292d663762b940d4f7a5f52bc2f5421be177e
Mayssam-Khlif <khlif.mk@proton.me>
2024-11-12 18:38:00 +0100

    (#18) URI Generation initial impl

--- src/gen.cc
+++ src/gen.cc
@@ -1,13 +1,49 @@
+#include <sstream>
 #include "gen.h"
+#include "str.h"
+#include "val-ipv6.h"
 
 std::string URIGenerator::generate(const Uri &uri) {
-	std::string result;
-	result += uri.getScheme();
-	result += "://";
-	result += uri.getHost();
-	for (const auto &path: uri.getPath()) {
-		result += "/";
-		result += path;
+	std::ostringstream buffer;
+	if (uri.hasScheme()) {
+		buffer << uri.getScheme() << ':';
 	}
-	return result;
+	if (uri.hasAuthority()) {
+		buffer << "//";
+		if (!uri.getUserInfo().empty()) {
+			// buffer << EncodeElement(impl_->userInfo, USER_INFO_NOT_PCT_ENCODED) << '@';
+		}
+		if (!uri.getHost().empty()) {
+			// case when host is an IPv6 address
+			if (IPv6Validator::isValid(uri.getHost())) {
+				buffer << '[' << StringExtensions::toLower(uri.getHost()) << ']';
+			} else {
+				// buffer << EncodeElement(uri.getHost(), CharacterSets::REG_NAME_NOT_PCT_ENCODED);
+			}
+		}
+		if (uri.hasPort()) {
+			buffer << ':' << uri.getPort();
+		}
+	}
+	// Special case: absolute but otherwise empty path.
+	if (uri.isPathAbsolute() && uri.getPath().size() == 1) {
+		buffer << '/';
+	}
+
+	size_t i = 0;
+	for (const auto &segment: uri.getPath()) {
+		// buffer << EncodeElement(segment, CharacterSets::PCHAR_NOT_PCT_ENCODED);
+		if (i + 1 < uri.getPath().size()) {
+			buffer << '/';
+		}
+		++i;
+	}
+	if (uri.hasQuery()) {
+		// buffer << '?' << EncodeElement(uri.getQuery(), CharacterSets::QUERY_NOT_PCT_ENCODED_WITHOUT_PLUS);
+	}
+	if (uri.hasFragment()) {
+		// buffer << '#' << EncodeElement(uri.getFragment(), CharacterSets::QUERY_OR_FRAGMENT_NOT_PCT_ENCODED);
+	}
+	return buffer.str();
 }
+
