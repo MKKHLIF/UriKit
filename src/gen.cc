de278f6f34ab14c0ba9a53d472212ad17dc38569
Mayssam-Khlif <khlif.mk@proton.me>
2024-11-12 19:21:39 +0100

    (#20) Add percent encoding to string generation

--- src/gen.cc
+++ src/gen.cc
@@ -1,5 +1,8 @@
 #include <sstream>
 #include "gen.h"
+
+#include "encoder.h"
+#include "sets.h"
 #include "str.h"
 #include "val-ipv6.h"
 
@@ -11,14 +14,14 @@ std::string URIGenerator::generate(const Uri &uri) {
 	if (uri.hasAuthority()) {
 		buffer << "//";
 		if (!uri.getUserInfo().empty()) {
-			// buffer << EncodeElement(impl_->userInfo, USER_INFO_NOT_PCT_ENCODED) << '@';
+			buffer << PercentEncoder::encode(uri.getUserInfo(), CharacterSets::USER_INFO_NOT_PCT_ENCODED) << '@';
 		}
 		if (!uri.getHost().empty()) {
 			// case when host is an IPv6 address
 			if (IPv6Validator::isValid(uri.getHost())) {
 				buffer << '[' << StringExtensions::toLower(uri.getHost()) << ']';
 			} else {
-				// buffer << EncodeElement(uri.getHost(), CharacterSets::REG_NAME_NOT_PCT_ENCODED);
+				buffer << PercentEncoder::encode(uri.getHost(), CharacterSets::REG_NAME_NOT_PCT_ENCODED);
 			}
 		}
 		if (uri.hasPort()) {
@@ -32,17 +35,17 @@ std::string URIGenerator::generate(const Uri &uri) {
 
 	size_t i = 0;
 	for (const auto &segment: uri.getPath()) {
-		// buffer << EncodeElement(segment, CharacterSets::PCHAR_NOT_PCT_ENCODED);
+		buffer << PercentEncoder::encode(segment, CharacterSets::PCHAR_NOT_PCT_ENCODED);
 		if (i + 1 < uri.getPath().size()) {
 			buffer << '/';
 		}
 		++i;
 	}
 	if (uri.hasQuery()) {
-		// buffer << '?' << EncodeElement(uri.getQuery(), CharacterSets::QUERY_NOT_PCT_ENCODED_WITHOUT_PLUS);
+		buffer << '?' << PercentEncoder::encode(uri.getQuery(), CharacterSets::QUERY_NOT_PCT_ENCODED_WITHOUT_PLUS);
 	}
 	if (uri.hasFragment()) {
-		// buffer << '#' << EncodeElement(uri.getFragment(), CharacterSets::QUERY_OR_FRAGMENT_NOT_PCT_ENCODED);
+		buffer << '#' << PercentEncoder::encode(uri.getFragment(), CharacterSets::QUERY_OR_FRAGMENT_NOT_PCT_ENCODED);
 	}
 	return buffer.str();
 }
