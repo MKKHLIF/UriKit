e986040e534838c6cf736d85417773f441a4ba3d
Mayssam-Khlif <khlif.mk@proton.me>
2024-11-23 21:57:31 +0100

    fragment parser docs #34

--- src/internal/fragment-parser.h
+++ src/internal/fragment-parser.h
@@ -1,7 +1,23 @@
 #pragma once
 #include <string>
 
+/**
+ * @class FragmentParser
+ * @brief A class for parsing the fragment component of a URI.
+ *
+ * The `FragmentParser` class provides functionality to extract and decode the fragment part of a URI.
+ * The fragment is the part of the URI that comes after the `#` symbol, and this class allows parsing and
+ * decoding it while also updating the original URI.
+ */
 class FragmentParser {
 public:
-     [[nodiscard]] static bool parse(std::string &uri, std::string &fragment);
+    /**
+     * @brief Parses the fragment from a URI and decodes it.
+     * @param uri The URI to parse.
+     * @param fragment The string to store the parsed fragment.
+     * @return true if the fragment was successfully parsed and decoded, false otherwise.
+     * @details This method looks for a `#` symbol in the URI to identify the start of the fragment. If the
+     *          fragment is found, it is extracted and decoded. The URI is updated to remove the fragment part.
+     */
+    [[nodiscard]] static bool parse(std::string &uri, std::string &fragment);
 };
